2a65c1e39279ecc661b29fa6c8972b0f5e1fbef5
Carloslt <carloslt.proyectos@gmail.com>
2024-05-09 20:30:15 +0200

    🔨 task: add setdrag and removedrag in store

--- src/pages/02-objects/SingleTask.tsx
+++ src/pages/02-objects/SingleTask.tsx
@@ -1,18 +1,27 @@
-import { IoReorderTwoOutline } from "react-icons/io5";
+import { IoReorderThreeOutline } from "react-icons/io5";
 import { Task } from "../../interfaces/task.type";
+import { useTaskStore } from "../../stores/tasks/task.store";
 
 type SingleTaskProps = {
   task: Task;
 };
 
 export const SingleTask = ({ task }: SingleTaskProps) => {
+  const setDraggingTaskId = useTaskStore((state) => state.setDraggingTaskId);
+  const removeDraggingTaskId = useTaskStore((state) => state.removeDraggingTaskId);
+
   return (
-    <div className="flex items-center justify-between p-2 mt-5">
+    <div
+      draggable
+      onDragStart={() => setDraggingTaskId(task.id)}
+      onDragEnd={() => removeDraggingTaskId()}
+      className="flex items-center justify-between p-2 mt-5"
+    >
       <div className="flex items-center justify-center gap-2">
         <p className="text-base font-bold text-navy-700">{task.title}</p>
       </div>
       <span className="w-6 h-6 cursor-pointer text-navy-700">
-        <IoReorderTwoOutline />
+        <IoReorderThreeOutline />
       </span>
     </div>
   );
--- src/stores/tasks/task.store.ts
+++ src/stores/tasks/task.store.ts
@@ -1,10 +1,14 @@
 import { create, StateCreator } from "zustand";
+import { devtools } from "zustand/middleware";
 import { Task, TaskStatus } from "../../interfaces/task.type";
 
 type TaskState = {
   tasks: Record<string, Task>;
+  draggingTaskId?: string;
 
   getTaskByStatus: (status: TaskStatus) => Task[];
+  setDraggingTaskId: (taskId: string) => void;
+  removeDraggingTaskId: () => void;
 };
 
 const storeApi: StateCreator<TaskState> = (set, get) => ({
@@ -14,10 +18,19 @@ const storeApi: StateCreator<TaskState> = (set, get) => ({
     "ABC-3": { id: "ABC-3", title: "Task 3", status: TaskStatus.IN_PROGRESS },
     "ABC-4": { id: "ABC-4", title: "Task 4", status: TaskStatus.DONE },
   },
+  draggingTaskId: undefined,
 
   getTaskByStatus: (status: TaskStatus) => {
     return Object.values(get().tasks).filter((task) => task.status === status);
   },
+
+  setDraggingTaskId: (taskId: string) => {
+    set({ draggingTaskId: taskId });
+  },
+
+  removeDraggingTaskId: () => {
+    set({ draggingTaskId: undefined });
+  },
 });
 
-export const useTaskStore = create<TaskState>()(storeApi);
+export const useTaskStore = create<TaskState>()(devtools(storeApi));
